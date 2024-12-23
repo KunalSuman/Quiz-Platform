943db1bbddde13f8746734e840bea804c8842d37
Kunal <kunal23295@iiitd.ac.in>
2024-12-24 00:50:09 +0530

    Admin DOne

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -3,7 +3,7 @@ import { Question_Card } from '../Components/Question_Card'
 export const Home = () =>{
     return (
         <div className = "bg-myblue  min-h-screen flex items-center-top justify-center " >
-           <div className = "w-1/2 rounded-md">
+           <div className = "w-1/3 rounded-md">
                 <Question_Card/>
            </div>
         </div>
--- client/src/Test.tsx
+++
@@ -1,9 +0,0 @@
-import  React  from "react";
-
-export const Test = () => {
-    return (
-        <div className = "bg-black min-h-screen">
-            <h1>Test</h1>
-        </div>
-    )
-}
